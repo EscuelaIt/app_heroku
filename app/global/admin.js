d479e9f51c432ec59e7573b6988930f287410aa7
Alberto Basalo <albertobasalo@hotmail.com>
2017-12-28 20:45:44 +0100

    clase

--- app/global/admin.js
+++ app/global/admin.js
@@ -4,7 +4,7 @@ module.exports = (app, url) => {
   // api/pub/admin
   app.route(url).get((req, res) => {
     const processInfo = {
-      message: "Hola Escuela IT",
+      message: "Estamos en clase con Escuela IT",
       version: packageJSON.version,
       env: {
         dyno: process.env.DYNO,
